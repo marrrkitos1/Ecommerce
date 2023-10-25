3913f1be84cdfda11dbbaa27e0c153ef8bd80673
Markitos <immakroteves@gmail.com>
2023-10-25 19:18:42 -0300

    1

---
+++ carrito.js
@@ -0,0 +1,79 @@
+const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+const iconoCarrito = document.querySelector('.bi-cart');
+const carritoContainer = document.querySelector('.carrito-container');
+const precioTotalContainer = document.querySelector('.precio-total');
+
+function actualizarCarrito() {
+    carritoContainer.innerHTML = '';
+    carrito.forEach((producto, index) => {
+        const carritoItem = document.createElement('div');
+        carritoItem.classList.add('carrito-item');
+        carritoItem.innerHTML = `
+            <h2 class="item-title">${producto.title}</h2>
+            <p class="cantidad">Cantidad: <span class="cantidad">${producto.cantidad}</span></p>
+            <p class="precio">Precio: $${producto.price}</p>
+            <div class="img-div"><img src="" class="product-img"></div>
+            <button class="btn-delete" data-index="${index}">Eliminar producto</button>
+        `;
+        carritoContainer.appendChild(carritoItem);
+        
+        fetch(`https://fakestoreapi.com/products/${producto.id}`)
+            .then(response => response.json())
+            .then(product => {
+                const imgElement = carritoItem.querySelector('.product-img');
+                if (product.image) {
+                    imgElement.src = product.image;
+                }
+            })
+            .catch(error => console.log('Hubo un error al obtener la imagen del producto', error));
+    });
+    agregarEventListenersEliminar();
+    calcularPrecioTotal();
+    actualizarIconoCarrito();
+}
+
+
+function agregarEventListenersEliminar() {
+    const btnEliminar = document.querySelectorAll('.btn-delete');
+    btnEliminar.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            eliminarProducto(index);
+        });
+    });
+}
+
+function eliminarProducto(index) {
+    const productoEliminado = carrito[index];
+    if (productoEliminado) {
+        if (productoEliminado.cantidad > 1) {
+            productoEliminado.cantidad -= 1;
+        } else {
+            carrito.splice(index, 1);
+        }
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+        actualizarCarrito();
+    }
+}
+
+function calcularPrecioTotal() {
+    if (carrito.length === 0) {
+        precioTotalContainer.textContent = 'No hay productos en el carrito';
+    } else {
+        const precioTotal = carrito.reduce((total, producto) => {
+            return total + producto.price * producto.cantidad;
+        }, 0);
+        precioTotalContainer.textContent = `Precio Total: $${precioTotal.toFixed(2)}`;
+    }
+}
+
+function actualizarIconoCarrito() {
+    if (carrito.length > 0) {
+        iconoCarrito.classList.remove('bi-cart');
+        iconoCarrito.classList.add('bi-cart-fill');
+    } else {
+        iconoCarrito.classList.remove('bi-cart-fill');
+        iconoCarrito.classList.add('bi-cart');
+    }
+}
+
+actualizarCarrito();
---
+++ producto.js
@@ -0,0 +1,65 @@
+const URL = 'https://fakestoreapi.com/products';
+
+const urlParams = new URLSearchParams(window.location.search);
+const productId = urlParams.get('id');
+
+const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+const iconoCarrito = document.querySelector('.bi-cart');
+
+fetch(`${URL}/${productId}`)
+    .then(response => response.json())
+    .then(product => {
+        actualizarIconoCarrito();
+        if (product) {
+            const productDetailsContainer = document.querySelector('.product-details-container');
+            productDetailsContainer.innerHTML = `
+                <div class="product-left">
+                    <img src="${product.image}" alt="${product.title}" class="product-img">
+                </div>
+                <div class="product-right">
+                    <h1 class="product-h1">${product.title}</h1>
+                    <p class="product-p">Descripción: ${product.description}</p>
+                    <p class="product-price">Precio: $${product.price}</p>
+                    <div class="btn-div"><button class="product-btn">Agregar al carrito</button></div>
+                </div>
+            `;
+            const btnAgregarAlCarrito = document.querySelector('.product-btn');
+            btnAgregarAlCarrito.addEventListener('click', () => {
+                agregarAlCarrito(product);
+                actualizarIconoCarrito();
+            });
+        } else {
+            console.log('Producto no encontrado.');
+        }
+    })
+    .catch(error => console.log('Hubo un error al obtener los datos del producto', error));
+
+    function actualizarIconoCarrito() {
+        if (carrito.length > 0) {
+            iconoCarrito.classList.remove('bi-cart');
+            iconoCarrito.classList.add('bi-cart-fill');
+        } else {
+            iconoCarrito.classList.remove('bi-cart-fill');
+            iconoCarrito.classList.add('bi-cart');
+        }
+    }
+
+    function agregarAlCarrito(product) {
+        const carritoItem = carrito.find(item => item.id === product.id);
+        if (carritoItem) {
+            carritoItem.cantidad += 1;
+        } else {
+            carrito.push({
+                id: product.id,
+                title: product.title,
+                price: product.price,
+                cantidad: 1
+            });
+    
+            const imagenesEnCache = JSON.parse(localStorage.getItem('imagenesCache')) || {};
+            imagenesEnCache[product.id] = product.image;
+            localStorage.setItem('imagenesCache', JSON.stringify(imagenesEnCache));
+        }
+    
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+    }
